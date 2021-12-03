691afa6aa0d05d2adbb509de2a79db1ceb3eb039
sheenashahangian <sheenas@bu.edu>
2021-12-02 19:42:11 -0700

    work in progress

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        // image one data, including filenames and alt text
+        imageOneBeforeHover: "multimedia/sheena-adult-1.jpeg",
+        imageOneAltTxtBefore: "Sheena standing in a lake with the wind blowing in her hair",
+        imageOneAfterHover: "multimedia/sheena-child-1.jpeg",
+        imageOneAltTxtAfter: "Sheena and her sister holding hands and walking down the street",
+
+        // image two data, including filenames and alt text
+        imageTwoBeforeHover: "multimedia/sheena-adult-2.jpeg",
+        imageTwoAltTxtBefore: "Sheena and her sister smiling at the camera on her wedding day",
+        imageTwoAfterHover: "multimedia/sheena-child-2.jpeg",
+        imageTwoAltTxtAfter: "Sheena and her sister hugging",
+
+        // image three data, including filenames and alt text
+        imageThreeBeforeHover: "multimedia/sheena-adult-3.jpeg",
+        imageThreeAltTxtBefore: "Sheena sitting on a rock in the mountains",
+        imageThreeAfterHover: "multimedia/sheena-child-3.jpeg",
+        imageThreeAltTxtAfter: "Sheena as a child wearing rainbow goggles at the pool",
+
+        //image four data, including filenames and alt text
+        imageFourBeforeHover: "multimedia/sheena-adult-4.jpeg",
+        imageFourAltTxtBefore: "Sheena smiling and looking down",
+        imageFourAfterHover: "multimedia/sheena-child-4.jpeg",
+        imageFourAltTxtAfter: "Sheena wearing a captain's hat"
+    }
+})
